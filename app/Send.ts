9ab42c2cbcb709aa69cf4db6b4d5fe23153eb44d
Mahim <mahim.g03@gmail.com>
2023-12-10 02:56:40 +0530

    Send Email

--- app/Send.ts
+++ app/Send.ts
@@ -1,14 +1,42 @@
-'use client';
+// Import the axios library for making HTTP requests
+import axios from 'axios';
 
-import { ethers } from 'ethers'
-import { GelatoRelayPack } from '@safe-global/relay-kit'
-import Safe, { EthersAdapter } from '@safe-global/protocol-kit'
-import { MetaTransactionData, MetaTransactionOptions } from '@safe-global/safe-core-sdk-types'
+// Ensure you have the necessary imports for your Ethereum-related code
+import { ethers } from 'ethers';
+import { GelatoRelayPack } from '@safe-global/relay-kit';
+import Safe, { EthersAdapter } from '@safe-global/protocol-kit';
+import { MetaTransactionData, MetaTransactionOptions } from '@safe-global/safe-core-sdk-types';
 import Pay from './Pay';
 
-function Send() {
-    
+// Function to send a request to 'localhost:5000/send-mail'
+async function Send() {
+  try {
+    // Set up your Ethereum-related code here if needed
 
+    // Example meta transaction data and options
+    // const metaTransactionData: MetaTransactionData = {
+
+    // };
+
+    // const metaTransactionOptions: MetaTransactionOptions = {
+    //   // Your meta transaction options
+    // };
+
+    // Example URL to send the request to
+    const url = 'http://localhost:5000/send-mail';
+
+    const payload = {
+    //   data: metaTransactionData,
+    //   options: metaTransactionOptions,
+    };
+
+    // Make a POST request using axios
+    const response = await axios.post(url, payload);
+
+    // Handle the response as needed
+    console.log('Response:', response.data);
+  } catch (error) {
+    // Handle errors
+    console.error('Error:', error);
+  }
 }
-
-export default Send;
